2c4ed15f1add59e3e7c08e0659edd3f74b4ecafe
Tiel Van Hecke <tiel.vhecke@hotmail.com>
2018-10-24 09:23:30 +0200

    Use MVVM

--- Stads-App/MainPage.xaml.cs
+++ Stads-App/MainPage.xaml.cs
@@ -12,19 +12,16 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Stads_App.ViewModels;
 
 namespace Stads_App
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
