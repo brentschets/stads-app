5603fe18c1307ecaff2a0b73e6fbb7a410f6c0a1
Tiel Van Hecke <tiel.vhecke@hotmail.com>
2018-11-07 13:23:28 +0100

    Implement HomeViewModel

--- Stads-App/ViewModels/HomeViewModel.cs
+++ Stads-App/ViewModels/HomeViewModel.cs
@@ -1,6 +1,63 @@
-﻿namespace Stads_App.ViewModels
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Stads_App.Annotations;
+using Stads_App.Models;
+using Stads_App.Utils;
+
+namespace Stads_App.ViewModels
 {
-    public class HomeViewModel
+    public class HomeViewModel : INotifyPropertyChanged
     {
+        private static readonly StadsAppRestApiClient Client = new StadsAppRestApiClient();
+
+        private List<Store> _mostVisited;
+
+        public List<Store> MostVisited
+        {
+            get => _mostVisited;
+            set
+            {
+                _mostVisited = value;
+                OnPropertyChanged(nameof(MostVisited));
+            }
+        }
+
+        private bool _isLoaded;
+
+        public bool IsLoaded
+        {
+            get => _isLoaded;
+            set
+            {
+                _isLoaded = value;
+                OnPropertyChanged(nameof(IsLoaded));
+            }
+        }
+
+        public HomeViewModel()
+        {
+            IsLoaded = false;
+        }
+
+        private static async Task<List<Store>> GetMostVisitedAsync()
+        {
+            return await Client.GetListAsync<Store>("Stores/MostVisited");
+        }
+
+        public async void LoadDataAsync()
+        {
+            MostVisited = await GetMostVisitedAsync();
+            IsLoaded = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
