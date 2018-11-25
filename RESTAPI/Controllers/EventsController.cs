7552edc439670db933a76d38b713a9fcc48a455b
Tiel Van Hecke <tiel.vhecke@hotmail.com>
2018-11-25 21:12:20 +0100

    Remove unnecessary CRUD operations from REST API

--- RESTAPI/Controllers/EventsController.cs
+++ RESTAPI/Controllers/EventsController.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RESTAPI.Data;
-using RESTAPI.Models;
 
 namespace RESTAPI.Controllers
 {
@@ -19,33 +16,6 @@ namespace RESTAPI.Controllers
             _context = context;
         }
 
-        // GET: api/Events
-        [HttpGet]
-        public IEnumerable<Event> GetEvent()
-        {
-            return _context.Event.Include(e => e.Store).ThenInclude(s => s.Address).Include(e => e.Store)
-                .ThenInclude(s => s.Category);
-        }
-
-        // GET: api/Events/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetEvent([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var @event = await _context.Event.Include(e => e.Store).FirstOrDefaultAsync(e => e.EventId == id);
-
-            if (@event == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(@event);
-        }
-
         // GET: api/Events/Popular/10
         [HttpGet("Popular/{limit}")]
         public IActionResult GetPopular([FromRoute] int limit)
@@ -53,83 +23,6 @@ namespace RESTAPI.Controllers
             var res = _context.Event.Include(e => e.Store).ThenInclude(s => s.Address).Include(e => e.Store)
                 .ThenInclude(s => s.Category).OrderByDescending(e => e.Visited).Take(limit).ToList();
             return Ok(res);
-        }
-
-        // PUT: api/Events/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutEvent([FromRoute] int id, [FromBody] Event @event)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != @event.EventId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(@event).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!EventExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Events
-        [HttpPost]
-        public async Task<IActionResult> PostEvent([FromBody] Event @event)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            _context.Event.Add(@event);
-            await _context.SaveChangesAsync();
-
-            // ReSharper disable once Mvc.ActionNotResolved
-            return CreatedAtAction("GetEvent", new {id = @event.EventId}, @event);
-        }
-
-        // DELETE: api/Events/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteEvent([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var @event = await _context.Event.FindAsync(id);
-            if (@event == null)
-            {
-                return NotFound();
-            }
-
-            _context.Event.Remove(@event);
-            await _context.SaveChangesAsync();
-
-            return Ok(@event);
-        }
-
-        private bool EventExists(int id)
-        {
-            return _context.Event.Any(e => e.EventId == id);
         }
     }
 }
--- RESTAPI/Controllers/PromotionsController.cs
+++ RESTAPI/Controllers/PromotionsController.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RESTAPI.Data;
@@ -27,109 +26,12 @@ namespace RESTAPI.Controllers
                 .ThenInclude(s => s.Category);
         }
 
-        // GET: api/Promotions/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetPromotion([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var promotion = await _context.Promotion.Include(p => p.Store)
-                .FirstOrDefaultAsync(p => p.PromotionId == id);
-
-            if (promotion == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(promotion);
-        }
-
         // GET: api/Promotions/Popular/10
         [HttpGet("Popular/{limit}")]
         public IActionResult GetPopular([FromRoute] int limit)
         {
             var res = _context.Promotion.Include(p => p.Store).OrderByDescending(p => p.Visited).Take(limit).ToList();
             return Ok(res);
-        }
-
-        // PUT: api/Promotions/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutPromotion([FromRoute] int id, [FromBody] Promotion promotion)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != promotion.PromotionId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(promotion).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PromotionExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Promotions
-        [HttpPost]
-        public async Task<IActionResult> PostPromotion([FromBody] Promotion promotion)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            _context.Promotion.Add(promotion);
-            await _context.SaveChangesAsync();
-
-            // ReSharper disable once Mvc.ActionNotResolved
-            return CreatedAtAction("GetPromotion", new {id = promotion.PromotionId}, promotion);
-        }
-
-        // DELETE: api/Promotions/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePromotion([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var promotion = await _context.Promotion.FindAsync(id);
-            if (promotion == null)
-            {
-                return NotFound();
-            }
-
-            _context.Promotion.Remove(promotion);
-            await _context.SaveChangesAsync();
-
-            return Ok(promotion);
-        }
-
-        private bool PromotionExists(int id)
-        {
-            return _context.Promotion.Any(e => e.PromotionId == id);
         }
     }
 }
