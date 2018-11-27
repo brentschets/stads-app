68482aaebb603334285b8f0fd3f078a6805843c2
brentschets <brent.schets17@gmail.com>
2018-11-27 14:25:06 +0100

    add account page

---
+++ Stads-App/ViewModels/Account/AccountViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Stads_App.Annotations;
+
+namespace Stads_App.ViewModels.Account
+{
+    public class AccountViewModel : INotifyPropertyChanged
+    {
+        private string _username;
+        private string _firstName;
+        private string _lastName;
+
+        public string LastName
+        {
+            get => _lastName;
+            private set { _lastName = value; OnPropertyChanged(nameof(LastName)); }
+        }
+
+
+        public string FirstName
+        {
+            get => _firstName;
+            private set { _firstName = value; OnPropertyChanged(nameof(FirstName)); }
+        }
+
+
+        public string Username
+        {
+            get => _username;
+            private set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Stads-App/Views/Account/Account.xaml.cs
@@ -0,0 +1,35 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Stads_App.Annotations;
+
+namespace Stads_App.Views.Account
+{
+    public sealed partial class Account: INotifyPropertyChanged
+    {
+        private string _header;
+
+        public override string Header
+        {
+            get => _header;
+            protected set
+            {
+                _header = value;
+                OnPropertyChanged(nameof(Header));
+            }
+        }
+
+        public Account()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
