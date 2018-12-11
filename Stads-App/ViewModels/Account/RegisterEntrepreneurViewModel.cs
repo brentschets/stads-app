4dd344f446d1ba7c0f689d856c683a0baedfd8b6
brentschets <brent.schets17@gmail.com>
2018-12-11 17:32:32 +0100

    added a screen for entrepreneur registration

---
+++ Stads-App/ViewModels/Account/RegisterEntrepreneurViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Stads_App.Annotations;
+using Stads_App.Models;
+using Stads_App.Utils;
+
+namespace Stads_App.ViewModels.Account
+{
+    public sealed class RegisterEntrepreneurViewModel: INotifyPropertyChanged
+    {
+        public ICommand FileUpload => new RelayCommand(o => UploadFile());
+
+        private string _errorMsg;
+
+        public string ErrorMsg
+        {
+            get => _errorMsg;
+            private set
+            {
+                _errorMsg = value;
+                OnPropertyChanged(nameof(ErrorMsg));
+            }
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+        public string Description { get; set; }
+
+        public string NameEntrepreneur { get; set; }
+
+        private List<Category> _categories;
+
+        public List<Category> Categories {
+            get => _categories;
+            private set
+            {
+                _categories = value;
+                OnPropertyChanged(nameof(Categories));
+            }
+        }
+
+        public async void UploadFile()
+        {
+            var folderPicker = new Windows.Storage.Pickers.FileOpenPicker();
+            folderPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop;
+            folderPicker.FileTypeFilter.Add(".jpg");
+            folderPicker.FileTypeFilter.Add(".jpeg");
+            folderPicker.FileTypeFilter.Add(".png");
+
+            Windows.Storage.StorageFile folder = await folderPicker.PickSingleFileAsync();
+            if (folder != null)
+            {
+                // Application now has read/write access to all contents in the picked folder
+                // (including other sub-folder contents)
+                Windows.Storage.AccessCache.StorageApplicationPermissions.
+                    FutureAccessList.AddOrReplace("PickedFolderToken", folder);
+                Debug.WriteLine("file selected");
+
+            }
+        }
+
+        public RegisterEntrepreneurViewModel()
+        {
+
+        }
+
+        public async Task<List<Category>> GetCategoriesAsync()
+        {
+            return await StadsAppRestApiClient.Instance.GetListAsync<Category>("Categories");
+        }
+
+        public async Task LoadData()
+        {
+            Categories = await GetCategoriesAsync();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+
+}
---
+++ Stads-App/Views/Account/EntrepreneurRegister.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Stads_App.ViewModels.Account;
+
+namespace Stads_App.Views.Account
+{
+    public sealed partial class EntrepreneurRegister
+    {
+        public override string Header { get; protected set; } = "Handelaar Registreren";
+
+        private readonly RegisterEntrepreneurViewModel _viewModel;
+
+        public EntrepreneurRegister()
+        {
+            this.InitializeComponent();
+        _viewModel = new RegisterEntrepreneurViewModel();
+            DataContext = _viewModel;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            await _viewModel.LoadData();
+        }
+    }
+}
