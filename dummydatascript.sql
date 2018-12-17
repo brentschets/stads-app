04d70e0e4cd7edaafb533885a0ad88dfe9513300
Tiel Van Hecke <tiel.vhecke@hotmail.com>
2018-12-17 13:21:53 +0100

    Update dummy data

--- dummydatascript.sql
+++ dummydatascript.sql
@@ -74,13 +74,13 @@ INSERT INTO Store(StoreId, Name, Description, ImgPath, CategoryId) VALUES
 (9, 'Ellis Gourmet Burger', 'Ellis is het concept van een groep enthousiastelingen met één doel voor ogen: betere burgers serveren. Ja, we gebruiken absoluut de beste producten, maar verfijnen onze recepten ook continu met de hulp van bekende productspecialisten en sterrenchefs.', '/img/9.png', 1),
 (10, 'Lab9', 'Lab9 is uw Apple Premium Reseller & Service Provider in West-Vlaanderen en Oost-Vlaanderen.', '/img/10.png', 4),
 
-(11, 'De Prins Drinkt Koffie','De Prins drinkt koffie is een trendy, hippe koffiebar aan de rand van de stad, met een ruime mogelijkheid tot parkeren.','/img/11.png',1),
-(12, 'De Zwarte Kat','Sinds 1994 zijn Steven en Annick de gastheren van het restaurant, met zicht op het oudste Belfort der Nederlanden.','/img/12.png',1),
-(13, 'Den Den','Café Den Den in de Sint-Janstraat, onder carnavalisten bekend als het café waar je moét geweest zijn.','/img/13.png',1),
-(14, 'In ''t Filetpuurken','Midden in het centrum van Aalst bevindt zich Brasserie In''t Filetpurken. In''t Filetpurken is de ideale plaats om lekker te eten in een gezellig en warm kader.','/img/14.png',1),
+(11, 'De Prins Drinkt Koffie','De Prins drinkt koffie is een trendy, hippe koffiebar aan de rand van de stad, met een ruime mogelijkheid tot parkeren.','/img/11.jpg',1),
+(12, 'De Zwarte Kat','Sinds 1994 zijn Steven en Annick de gastheren van het restaurant, met zicht op het oudste Belfort der Nederlanden.','/img/12.jpg',1),
+(13, 'Den Den','Café Den Den in de Sint-Janstraat, onder carnavalisten bekend als het café waar je moét geweest zijn.','/img/13.jpg',1),
+(14, 'In ''t Filetpuurken','Midden in het centrum van Aalst bevindt zich Brasserie In''t Filetpurken. In''t Filetpurken is de ideale plaats om lekker te eten in een gezellig en warm kader.','/img/14.jpg',1),
 
-(15, 'De Slegte', 'Op zoek naar een origineel boek dat niet in iedere boekwinkel in de top tien staat? Kijk dan eens bij Boekhandel De Slegte. Wij hebben een uitgebreide collectie bijzondere en voordelige boeken. Zowel in onze winkels als in de Vlaamse webwinkel. Ook kopen wij uw boeken graag in.','/img/15.png',2),
-(16, 'Club', 'Boekenhandel Club is een van de weinige die er nog is. Kom zeker eens langs bij ons!', '/img/16.png', 2),
+(15, 'De Slegte', 'Op zoek naar een origineel boek dat niet in iedere boekwinkel in de top tien staat? Kijk dan eens bij Boekhandel De Slegte. Wij hebben een uitgebreide collectie bijzondere en voordelige boeken. Zowel in onze winkels als in de Vlaamse webwinkel. Ook kopen wij uw boeken graag in.','/img/15.jpg',2),
+(16, 'Club', 'Boekenhandel Club is een van de weinige die er nog is. Kom zeker eens langs bij ons!', '/img/16.jpg', 2),
 (17, 'De Press shop', 'Press Shop is een krantenwinkel waar u terecht kan voor pers, rookwaren, snoepgoed, boeken, Nationale Loterij.','/img/17.png',2),
 (18, 'Dallas', 'Dit is een krantenwinkel waar u terecht kan voor bijna alles.','/img/18.png',2),
 
@@ -89,23 +89,23 @@ INSERT INTO Store(StoreId, Name, Description, ImgPath, CategoryId) VALUES
 (21, 'Dunkie', 'Breng vrijblijvend een bezoekje aan onze winkel in Aalst, missschien hebben wij het hondje dat u zoekt.','/img/21.png',3),
 (22, 'Schockaert Rud', 'Diensten en producten: Dierenvoeding, Tuinbenodigdheden, Bloemen -en groenteplanten, Duivenvoer, Paardenvoer.','/img/22.png',3),
 
-(23, 'Van Den Borre', 'De specialist in TV, video, hifi, elektro, multimedia en GSM in België. Gemakkelijk te vergelijken en online kopen uit een ruime keuze.','/img/23.png',4),
-(24, 'Mediamarkt', 'Computer & Multimedia - Telefoon & Navigatie - Televisie & Audio - Foto & Video - Keuken - Huishouden & Verzorging - Gaming & Entertainment - ...','/img/24.png',4),
+(23, 'Van Den Borre', 'De specialist in TV, video, hifi, elektro, multimedia en GSM in België. Gemakkelijk te vergelijken en online kopen uit een ruime keuze.','/img/23.jpg',4),
+(24, 'Mediamarkt', 'Computer & Multimedia - Telefoon & Navigatie - Televisie & Audio - Foto & Video - Keuken - Huishouden & Verzorging - Gaming & Entertainment - ...','/img/24.jpg',4),
 (25, 'Selexion', 'Eenvoudig online vergelijken & bestellen op onze Selexion webshop, de specialist in TV, Audio, Foto, IT, Telefonie, Elektro & Cookware. Ontdek ons gamma!','/img/25.png',4),
 (26, 'Electrostock', 'Bij ElectroStock hebben we bijna alles op voorraad in de winkel! Wilt u meteen van uw aankoop kunnen genieten, dan moet u bij ElectroStock zijn!','/img/26.png',4),
 
 (27, 'C&A', 'Ontdek modieuze kleding voor dames, heren en kinderen bij C&A! Topkwaliteit ? Duurzame producten.','/img/27.png',5),
-(28, 'CoolCat', 'Kleding in allerlei stijlen, kleuren & prints. Go get yours ? Shop nu! Snelle Levering. Gratis Ophalen in Winkels. Typen: New, Sale, Basics, Bestsellers, Party wear, Festival outfits.','/img/28.png',5),
+(28, 'CoolCat', 'Kleding in allerlei stijlen, kleuren & prints. Go get yours ? Shop nu! Snelle Levering. Gratis Ophalen in Winkels. Typen: New, Sale, Basics, Bestsellers, Party wear, Festival outfits.','/img/28.jpg',5),
 (29, 'TIFFANYS Dames', 'Bij Dressed by Tiffanys & Trent kan je steeds terecht voor sterke modemerken. Beleef je mode bij ons en bezoek onze merken pagina.','/img/29.png',5),
 (30, 'Sint Elooi', 'Sint Elooi is een gevestigde waarde in het centrum van Aalst. Deze familiezaak bestaat al meer dan 70 jaar.','/img/30.png',5),
 
 (31, 'Van Praet Interieur & Kantoorinrichting', 'Bij VAN PRAET kunt u terecht om uw woonwerelden volledig te laten aanpassen aan uw wensen en verwachtingen. Ons team van ervaren interieurarchitecten zullen u graag begeleiden en adviseren, geheel kosteloos, om samen de mooiste meubelstukken in te passen in uw plannen.','/img/31.png',6),
 (32, 'Euromeubels', 'Voor alle meubels moet u bij Euromeubel!','/img/32.png',6),
 (33, 'Merckx Aalst Teak', 'Merckx Meubelen. In en outdoor meubelen. Extraordinary furniture for extraordinary people. Teak meubelen.','/img/33.png',6),
-(34, 'Leen Bakker', 'Mooi wonen voor lage prijzen. En altijd verrassende acties. Bezoek één van onze Leen Bakker winkels of bestel online.','/img/34.png',6),
+(34, 'Leen Bakker', 'Mooi wonen voor lage prijzen. En altijd verrassende acties. Bezoek één van onze Leen Bakker winkels of bestel online.','/img/34.jpg',6),
 
 (35, 'De Speelvogel', 'Wij bieden enkel leuk, kwalitatief en verantwoord speelgoed.','/img/35.png',7),
-(36, 'Bart Smit Speelgoedwinkel', 'Het beste speelgoed vindt u in de Bart Smit.','/img/36.png',7),
+(36, 'Bart Smit Speelgoedwinkel', 'Het beste speelgoed vindt u in de Bart Smit.','/img/36.jpg',7),
 (37, 'Modelbouw', 'Enkel en alleen de gevel is prachtig! Binnen is het een waar paradijs voor de modelbouwers.','/img/37.png',7),
 (38, 'Fox & Cie Aalst', 'Fox & Cie is de beste speelgoed winkel in Aalst.','/img/38.png',7),
 
@@ -118,14 +118,14 @@ SET IDENTITY_INSERT Store OFF
 SET IDENTITY_INSERT Establishment ON
 INSERT INTO Establishment(EstablishmentId, ImgPath, Visited, AddressId, StoreId) VALUES
 (2, '/img/establishments/2.jpg', 84, 2, 2),
-(3, '/img/establishments/3.png', 25, 3, 3),
+(3, '/img/establishments/3.PNG', 25, 3, 3),
 (4, '/img/establishments/4.jpg', 13, 4, 3),
 (5, '/img/establishments/5.jpg', 136, 5, 4),
 (6, '/img/establishments/6.jpg', 87, 6, 5),
 (1, '/img/establishments/1.jpg', 156, 1, 1),
-(7, '/img/establishments/7.png', 45, 7, 6),
+(7, '/img/establishments/7.PNG', 45, 7, 6),
 (9, '/img/establishments/9.JPG', 150, 9, 8),
-(8, '/img/establishments/8.png', 96, 8, 7),
+(8, '/img/establishments/8.PNG', 96, 8, 7),
 (10, '/img/establishments/10.jpg', 236, 10, 9),
 (11, '/img/establishments/11.jpg', 71, 11, 10),
 
@@ -137,17 +137,17 @@ INSERT INTO Establishment(EstablishmentId, ImgPath, Visited, AddressId, StoreId)
 (16, '/img/establishments/16.jpg', 43,16,16),
 (17, '/img/establishments/17.jpg', 45, 17,15),
 (18, '/img/establishments/18.jpg', 45, 18,17),
-(19, '/img/establishments/19.jpg', 45, 19,18),
+(19, '/img/establishments/19.png', 45, 19,18),
 
 (20, '/img/establishments/20.png', 45, 20, 19),
-(21, '/img/establishments/21.jpg', 45, 21, 20),
-(22, '/img/establishments/22.jpg', 45, 22, 21),
-(23, '/img/establishments/23.jpg', 45, 23, 22),
+(21, '/img/establishments/21.png', 45, 21, 20),
+(22, '/img/establishments/22.png', 45, 22, 21),
+(23, '/img/establishments/23.png', 45, 23, 22),
 
 (24, '/img/establishments/24.jpg', 45, 24, 23),
 (25, '/img/establishments/25.jpg', 45, 25, 24),
-(26, '/img/establishments/26.jpg', 45, 26, 25),
-(27, '/img/establishments/27.jpg', 45, 27, 26),
+(26, '/img/establishments/26.png', 45, 26, 25),
+(27, '/img/establishments/27.png', 45, 27, 26),
 
 (28, '/img/establishments/28.jpg', 45, 28, 27),
 (29, '/img/establishments/29.jpg', 45, 29, 28),
@@ -185,8 +185,3 @@ INSERT INTO Promotion(PromotionId, Name, Visited, StoreId) VALUES
 (3, 'T-Shirts voor maar 2 euro', 50, 42),
 (4, '2 zakken hondenbrokken en een gratis speeltje', 60, 19)
 SET IDENTITY_INSERT Promotion OFF
-
-INSERT INTO UserEstablishment (UserId, EstablishmentId) VALUES
-(1, 3),
-(1, 10),
-(1, 11)
